70d93560e77be1e6004f266181113cdbfb7b661a
dusanov <n0mid@mail.ru>
2018-01-29 11:07:50 +0400

    Clear files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   public books: any[] = [];
